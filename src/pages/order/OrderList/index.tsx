67ce093c81f9629a1e7f1f575dc4b17dacca7ac8
forever <ecmascript2020@163.com>
2026-03-30 16:48:27 +0800

    test

--- src/pages/order/OrderList/index.tsx
+++ src/pages/order/OrderList/index.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-import { PageContainer } from '@ant-design/pro-components';
-import { Form, Spin } from 'antd';
+import {PageContainer} from '@ant-design/pro-components';
+import {Form, Spin} from 'antd';
 
 import OrderFilterForm from '@/features/order/components/OrderFilterForm';
 import OrderListWithModal from '@/features/order/components/OrderListWithModal';
 import useOrderList from '@/features/order/hooks/useOrderList';
 
 const OrderListPage: React.FC = () => {
-  const [form] = Form.useForm();
+    const [form] = Form.useForm();
 
-  const { loading, orderListInfo, onPageChange, onSearch, refresh } =
-    useOrderList({
-      form,
-    });
+    const {loading, orderListInfo, onPageChange, onSearch, refresh} =
+        useOrderList({
+            form,
+        });
+    useEffect(() => {
+        a();
+    }, [])
 
-  return (
-    <Spin spinning={loading}>
-      <PageContainer>
-        <OrderFilterForm onSearch={onSearch} form={form} />
-        <OrderListWithModal
-          orderListInfo={orderListInfo}
-          onPageChange={onPageChange}
-          onRefresh={refresh}
-        />
-      </PageContainer>
-    </Spin>
-  );
+    function a() {
+        return 1 + 1
+    }
+
+    return (
+        <Spin spinning={loading}>
+            <PageContainer>
+                <OrderFilterForm onSearch={onSearch} form={form}/>
+                <OrderListWithModal
+                    orderListInfo={orderListInfo}
+                    onPageChange={onPageChange}
+                    onRefresh={refresh}
+                />
+            </PageContainer>
+        </Spin>
+    );
 };
 
 export default OrderListPage;
