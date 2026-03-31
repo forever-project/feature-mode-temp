d2e9357cb64c833506c4f0f2c4b64a6bce560769
forever <ecmascript2020@163.com>
2026-03-31 09:27:16 +0800

    test

--- src/pages/order/OrderList/index.tsx
+++ src/pages/order/OrderList/index.tsx
@@ -14,6 +14,13 @@ const OrderListPage: React.FC = () => {
         useOrderList({
             form,
         });
+    useEffect(() => {
+        a();
+    }, [])
+
+    function a() {
+        return 1 + 12
+    }
 
     return (
         <Spin spinning={loading}>
