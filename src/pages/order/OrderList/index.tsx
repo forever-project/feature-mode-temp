8b0bcf8dd7a48b677ee18749ac7f65f4d6115c75
forever <ecmascript2020@163.com>
2026-04-02 09:51:20 +0800

    fix

--- src/pages/order/OrderList/index.tsx
+++ src/pages/order/OrderList/index.tsx
@@ -15,7 +15,10 @@ const OrderListPage: React.FC = () => {
             form,
         });
 
-
+    useEffect(()=>{
+        a();
+    },[])
+    function a(){}
 
     return (
         <Spin spinning={loading}>
